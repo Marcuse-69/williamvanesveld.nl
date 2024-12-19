b32b51810930c7dbe492e1a18690315a64eb3cd9
MRW van Esveld <marcus.vanesveld@student.uva.nl>
2024-12-19 20:25:27 +0100

    feat: Add loading state for experimental interface

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="text-4xl font-light animate-pulse">
+        Loading experimental interface...
+      </div>
+    </div>
+  );
+}
