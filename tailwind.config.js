462fe8f455d73b0f6e00bbabdb29f2e54c30af9d
MRW van Esveld <marcus.vanesveld@student.uva.nl>
2024-12-20 23:39:32 +0100

    feat: Add remaining configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      cursor: {
-        crosshair: 'crosshair',
+      animation: {
+        glitch: 'glitch 0.3s cubic-bezier(.25,.46,.45,.94) both infinite',
+      },
+      keyframes: {
+        glitch: {
+          '0%': { transform: 'translate(0)' },
+          '20%': { transform: 'translate(-2px, 2px)' },
+          '40%': { transform: 'translate(-2px, -2px)' },
+          '60%': { transform: 'translate(2px, 2px)' },
+          '80%': { transform: 'translate(2px, -2px)' },
+          '100%': { transform: 'translate(0)' },
+        },
       },
     },
   },
